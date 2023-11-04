a88dde07bfbafeb34a509dc3af3d973ab7f6f9eb
实验室 <875241454@qq.com>
2023-11-04 15:05:55 +0800

    2023/11/4

--- test.cpp
+++ test.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
 using namespace std;
+class A{
+    friend class B;
+private:
+    int x=1;
+};
+
+class B{
+    friend class C;
+private:
+    int x=2;
+};
+
+class C{
+public:
+    void visitB(){
+        B b;
+        cout<<b.x<<endl;
+    }
+    // void visitA(){
+    //     A a;
+    //     cout<<a.x<<endl;
+    // }
+private:
+    int x=3;
+};
+
 int main(){
-    int x;
-    x = x;
-    cout<<x<<endl;
+    
     system("pause");
     return 0;
 }
