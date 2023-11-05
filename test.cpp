d435045f2f101f20057d15d557fb4dceaec1d91e
实验室 <875241454@qq.com>
2023-11-05 16:05:50 +0800

    2023/11/5

--- test.cpp
+++ test.cpp
@@ -1,33 +1,19 @@
 #include <iostream>
 using namespace std;
-class A{
-    friend class B;
-private:
-    int x=1;
-};
 
-class B{
-    friend class C;
-private:
-    int x=2;
-};
-
-class C{
-public:
-    void visitB(){
-        B b;
-        cout<<b.x<<endl;
-    }
-    // void visitA(){
-    //     A a;
-    //     cout<<a.x<<endl;
-    // }
-private:
-    int x=3;
-};
 
 int main(){
-    
+    int a = 1;
+    int & b = a;
+    int& c = a;
+    int &d = a;
+    cout<<a<<" "<<b<<" "<<c<<" "<<d<<endl;
+    b = 2;
+    cout<<a<<" "<<b<<" "<<c<<" "<<d<<endl;
+    c = 3;
+    cout<<a<<" "<<b<<" "<<c<<" "<<d<<endl;
+    d = 4;
+    cout<<a<<" "<<b<<" "<<c<<" "<<d<<endl;
     system("pause");
     return 0;
 }
