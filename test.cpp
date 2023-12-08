49759d5a374e8a4850b454b250826841b08c08f8
T0kisaki-Kurumi <875241454@qq.com>
2023-12-08 22:12:13 +0800

    2023/12/8

--- test.cpp
+++ test.cpp
@@ -1,50 +1,19 @@
 #include <iostream>
 using namespace std;
 
-class MyException{
+class base{
 public:
-    MyException(){
-        cout<<"MyException构造"<<endl;
-    }
-    MyException(const MyException& e){
-        cout<<"MyException拷贝构造"<<endl;
-    }
-
-    ~MyException(){
-        cout<<"MyException析构"<<endl;
+    virtual void print(){
+        cout<<1<<endl;
     }
 };
 
-MyException doWork1(){
-    return MyException();
-}
+class son:public base{
 
-void test1(){
-    MyException e(doWork1());
-    cout<<"正常时，return异常类对象"<<endl;
-}
-
-void doWork2(){
-    throw MyException();
-}
-
-void test2(){
-    try{
-        doWork2();
-    }
-    catch(MyException e){
-        cout<<"异常时，catch异常类对象"<<endl;
-    }
-}
+};
  
 int main(){
-    cout<<"test1:"<<endl;
-    test1();
-
-    cout<<endl<<endl;
-
-    cout<<"test2:"<<endl;
-    test2();
+    son().print();
 
     system("pause");
     return 0;
