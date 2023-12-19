7e4f7bf3b2b6b5f713a3105f0d3f9ffcc208a036
T0kisaki-Kurumi <875241454@qq.com>
2023-12-19 21:12:11 +0800

    2023/12/19

---
+++ code/77-05 string容器（上）/77-05 string容器（上）.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+void test1(){
+    
+}
+
+void test2(){
+    
+}
+
+void test3(){
+    
+}
+
+void test4(){
+    
+}
+
+void test5(){
+    
+}
+
+int main(){
+    test1();
+    // test2();
+    // test3();
+    // test4();
+    // test5();
+
+    system("pause");
+    return 0;
+}
