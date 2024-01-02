f76865287045cc58a3faa18222b1a92983130417
T0kisaki-Kurumi <875241454@qq.com>
2024-01-02 22:00:27 +0800

    2024/1/2

--- code/79-07 vector容器（上）/79-07 vector容器（上）.cpp
+++ code/79-07 vector容器（上）/79-07 vector容器（上）.cpp
@@ -25,16 +25,36 @@ void test2(){
     printVector(v2);
     vector<int> v3(v2.begin()+5, v2.end());
     printVector(v3);
+    vector<int> v4;
+    v4.assign(v2.begin(), v2.begin()+5);
+    printVector(v4);
 }
 
 void test3(){
-    
+    vector<int> v1{1,2,3};
+    vector<int> v2{4,5};
+    v1.swap(v2);
+    printVector(v1);
+    printVector(v2);
+}
+
+void test4(){
+    vector<int> v1{1,2,3};
+    cout<<v1.size()<<endl;
+    printVector(v1);
+    v1.resize(5);
+    cout<<v1.size()<<endl;
+    printVector(v1);
+    v1.resize(10, 6);
+    cout<<v1.size()<<endl;
+    printVector(v1);
 }
 
 int main(){
     // test1();
-    test2();
+    // test2();
     // test3();
+    test4();
 
     system("pause");
     return 0;
