716f7d9622e5a4085e3a7d7afa46f66514096545
T0kisaki-Kurumi <875241454@qq.com>
2024-01-09 21:53:02 +0800

    2024/1/9

--- code/81-09 deque容器以及作业布置/81-09 deque容器以及作业布置.cpp
+++ code/81-09 deque容器以及作业布置/81-09 deque容器以及作业布置.cpp
@@ -13,6 +13,9 @@ void printDeque(const deque<T>& d){
 void test1(){
     deque<int> d{1,2,3,4,5};
     printDeque(d);
+    cout<<d[3]<<endl;
+    d.push_front(6);
+    cout<<d[3]<<endl;
 }
 
 int main(){
